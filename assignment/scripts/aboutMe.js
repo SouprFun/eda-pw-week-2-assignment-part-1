ffcf5f96187da1a90311c6a3571dad65132538cb
Soup <bry123ftw@gmail.com>
2022-03-28 19:51:32 -0500

    up to pet math

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -15,15 +15,17 @@ let luckyNumber = 18
 // Refer back to the videos if you need help with this one.
 console.log( "My name is " + FullName + " and I think " + luckyNumber + " is a winner!");
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = "Ramen";
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 1
+console.log ( friendsPets );
 // 11 - Add two pets to your `pets` variable
-
+friendsPets += 2
+console.log(friendsPets );
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
